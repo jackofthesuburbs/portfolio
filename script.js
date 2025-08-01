302e613946d522afbc8f1c80e417592c6a239f7d
gary-martin <48495597+jackofthesuburbs@users.noreply.github.com>
2025-08-01 21:19:16 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Get today's date
-const d = new Date();
-const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
-const month = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
-const day = new Intl.DateTimeFormat('en', { day: 'numeric' }).format(d);
-const weekday = new Intl.DateTimeFormat('en', { weekday: 'short' }).format(d);
-
-document.querySelector('.time').innerText = `${weekday} ${month} ${day}, ${year}`;
